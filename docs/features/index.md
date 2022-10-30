27812965b7c932275a7796489ecdd58307cc5444
NathanTW0219 <nelsan9789@gmail.com>
2022-10-30 15:27:28 +0800

    Done getting started

---
+++ docs/features/index.md
@@ -0,0 +1,11 @@
+---
+title: 特色功能
+description: 這個章節會講述幾個 YEE式機器龍 的主打特色功能
+---
+
+## 目錄
+在這個章節，我們會討論幾個 YEE式機器龍 的主打特色功能，像是...
+
+### 動態語音
+
+![動態語音演示](/img/docs/features/dynamic-voice-channels.gif)
--- sidebars.js
+++ sidebars.js
@@ -10,45 +10,57 @@
  */
 
 module.exports = {
-	yeecordSidebar: [
-		'index',
-		'migrate-to-v4',
-		{
-			type: 'category',
-			label: '指令',
-			link: { type: 'doc', id: 'commands/index' },
-			items: [
-				'commands/index',
-				'commands/list',
-				{
-					type: 'category',
-					label: '指令參照',
-					link: { type: 'generated-index' },
-					items: [
-						'commands/reference/general'
-					]
-				}
-			]
-		},
-		{
-			type: 'category',
-			label: "RPG系統全攻略",
-			link: { type: 'doc', id: 'rpg/index' },
-			items: [
-				'rpg/index',
-				'rpg/jobs',
-				'rpg/shop',
-				'rpg/make'
-			]
-		},
-		'role',
-		'poll',
-		'dvc',
-		'join-message',
-		'ticket',
-		'faq',
-		'chat',
-		'credit',
-		'copyright'
-	]
+  yeecordSidebar: [
+    'index',
+    'getting-started',
+    {
+      type: 'category',
+      label: '特色功能',
+      link: { type: 'doc', id: 'features/index' },
+      items: [
+        {
+          type: 'doc',
+          label: '目錄',
+					id: 'features/index'
+        }
+      ]
+    }
+    // {
+    // 	type: 'category',
+    // 	label: '指令',
+    // 	link: { type: 'doc', id: 'commands/index' },
+    // 	items: [
+    // 		'commands/index',
+    // 		'commands/list',
+    // 		{
+    // 			type: 'category',
+    // 			label: '指令參照',
+    // 			link: { type: 'generated-index' },
+    // 			items: [
+    // 				'commands/reference/general'
+    // 			]
+    // 		}
+    // 	]
+    // },
+    // {
+    // 	type: 'category',
+    // 	label: "RPG系統全攻略",
+    // 	link: { type: 'doc', id: 'rpg/index' },
+    // 	items: [
+    // 		'rpg/index',
+    // 		'rpg/jobs',
+    // 		'rpg/shop',
+    // 		'rpg/make'
+    // 	]
+    // },
+    // 'role',
+    // 'poll',
+    // 'dvc',
+    // 'join-message',
+    // 'ticket',
+    // 'faq',
+    // 'chat',
+    // 'credit',
+    // 'copyright'
+  ]
 };
